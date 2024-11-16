4c82cd2a84d7547534c132564b7425b0672662c8
Fopa Tsopdieu Cedric Gueubruers <fopa.tsopdieu@lao-sarl.cm>
2024-11-16 23:36:11 +0100

    setUp

---
+++ src/test/java/cm/gueubruers/keycloakadapter/KeycloakAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.gueubruers.keycloakadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakAdapterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
